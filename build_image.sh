a4cbbbd3af85aa7f8a594c8c8df1f0d3e9179ecd
Alan King <alanking@renci.org>
2020-04-15 16:21:01 -0400

    [#4] Add --no-cache option to build script

--- build_image.sh
+++ build_image.sh
@@ -8,13 +8,14 @@ Builds a new docker image.
 
 Example:
 
-    ./build_image.sh --image-name <arg> --irods-repo <arg> --irods-branch <arg> ...
+    ./build_image.sh --image-name <arg> ...
 
 Available options:
 
     --image-name            The name of the new docker image
     --cmake-path            Full path to the CMake binary (e.g. /opt/irods-externals/cmakeX.X.X/bin)
     --dockerfile            Dockerfile to build (default: ./Dockerfile)
+    --no-cache              Do not use cached images when building
     -h, --help              This message
 _EOF_
     exit
@@ -23,6 +24,7 @@ _EOF_
 # defaults
 image_name=irods_ub16_builder
 build_args=
+docker_build_args=
 dockerfile=Dockerfile.ub16
 
 while [ -n "$1" ]; do
@@ -30,9 +32,10 @@ while [ -n "$1" ]; do
         --image-name)       shift; image_name=${1};;
         --cmake-path)       shift; build_args="$build_args --build-arg cmake_path=${1}";;
         --dockerfile)       shift; dockerfile=${1};;
+        --no-cache)         shift; docker_build_args="--no-cache";;
         -h|--help)          usage;;
     esac
     shift
 done
 
-docker build -f $dockerfile -t $image_name $build_args .
+docker build $docker_build_args -f $dockerfile -t $image_name $build_args .
